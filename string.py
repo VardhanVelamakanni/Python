5aaf94915cefe4bf4a7b80bd7808ec6b9143afeb
Vardhan velamakanni <159052557+VardhanVelamakanni@users.noreply.github.com>
2025-02-03 00:34:05 +0530

    commit final string basics

--- string.py
+++ string.py
@@ -1,7 +1,10 @@
 
  # type: ignore
  # 
-string1="this is a string"
+string1="this is a string"#example of a string declaration
 #for the line to be printed in new line
 string2="this string is in first line\nthis string is in new line"
 print(string2)
+#for a tab space btween
+string3="before gap\tafter gap"
+print(string3)
