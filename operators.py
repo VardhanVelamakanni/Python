a2e1df7807a84d46f1d2f28a80608a35bd733265
Vardhan velamakanni <159052557+VardhanVelamakanni@users.noreply.github.com>
2025-02-02 23:56:04 +0530

    commit type conversion

--- operators.py
+++ operators.py
@@ -25,4 +25,24 @@ print(power)
 x=(10)
 x+=10
 print("num", x)
+#similar for every other assignment operator
+
+#Relational operators is for relating
+s=10
+d=4
+if (s==d):        #if else statements(learned them)
+    print("equal")
+else:
+    print("no")
+
+#logical operators
+#AND,OR,NOT
+
+f=50
+g=30
+print(not(f>g))#output will be flase as the given condtion is true and not of true is flase
+val1=True
+val2=False
+print("And operator",val1 and val2)#only gives true if any of the values of the variable is true..or it will only result in false
+print("Or operator" ,val1 or val2)#results in true of any of the value of the varaiable is true
 
---
+++ typeconversion.py
@@ -0,0 +1,17 @@
+#type conversion
+#a="25"
+#b=50
+#sum=(a+b)
+#print(type(a))
+#print(sum)
+#that code results in a error as it cant concatinate a string with a interger type or float type
+a=int("25")#type conversion
+b=50
+sum=(a+b)
+print(sum)
+#but
+#a=int("vardhan")
+#b=50
+#sum=(a+b)
+#print(sum)
+#that doesnt work as the data tpe that iam trying to type convert ios not in the form of integer
