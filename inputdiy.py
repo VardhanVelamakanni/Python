311cd03f6fe332279a4d2cda2cce66d368323140
Vardhan velamakanni <159052557+VardhanVelamakanni@users.noreply.github.com>
2025-02-03 00:26:03 +0530

    commitdiy

---
+++ inputdiy.py
@@ -0,0 +1,16 @@
+#DIY
+#Write a program to input two numbers and print their sum
+value1=int(input("enter the first number"))
+value2=int(input("enter the second nummber"))
+sum=(value1+value2)
+print(sum)
+#write a program to take inputs of a side of square and pront its area
+a=int(input("enter the side of the number"))
+area=(a**2)
+print("area",area)
+#write a program to inoput 2 floating point numbers and print their average
+a=float(input("enter the first loating decimal number"))
+b=float(input("enter the second loating decimal number"))
+
+avrg=((a+b)/2)
+print("average is ",avrg)
