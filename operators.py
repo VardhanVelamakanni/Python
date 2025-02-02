493cb21bf53d6fd29554df376b036407593519f1
Vardhan velamakanni <159052557+VardhanVelamakanni@users.noreply.github.com>
2025-02-02 23:23:28 +0530

    commit operators 1

---
+++ operators.py
@@ -0,0 +1,28 @@
+#operators
+#Arithmetic Operators ( +,-,*,/,%, ** )
+#Relational / Comparison Operators ( == , != , > , <, >=, <= )
+#Assignment Operators ( = , +=, -= , *= , /=, %=, **= )
+#Logical Operators ( not, and , or )
+
+#Arithmetic oprators
+a=10
+b=20
+sum=(a+b)
+sub=(a-b)
+mul=(a*b)
+float=(a/b)
+modulo=(a%b)
+power=(a**b)
+
+print(sum)
+print(sub)
+print(mul)
+print(float)
+print(modulo)
+print(power)
+ 
+#assignment operator
+x=(10)
+x+=10
+print("num", x)
+
