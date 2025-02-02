9cc53291b8c81e20f34a2a47047f43c13c94484e
Vardhan velamakanni <159052557+VardhanVelamakanni@users.noreply.github.com>
2025-02-03 00:27:45 +0530

    commit final diy(class1)

--- inputdiy.py
+++ inputdiy.py
@@ -5,12 +5,16 @@ value2=int(input("enter the second nummber"))
 sum=(value1+value2)
 print(sum)
 #write a program to take inputs of a side of square and pront its area
-a=int(input("enter the side of the number"))
+a=int(input("enter the side of the square"))
 area=(a**2)
 print("area",area)
 #write a program to inoput 2 floating point numbers and print their average
 a=float(input("enter the first loating decimal number"))
 b=float(input("enter the second loating decimal number"))
-
 avrg=((a+b)/2)
 print("average is ",avrg)
+#WAP to input 2 int numbers, val1 and val2.
+#Print True if a is greater than or equal to b. If not print False.
+val1=int(input("enter the first number"))
+val2=int(input("enter the second number"))
+print(a>=b)
