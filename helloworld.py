d1f204d4ae15bbb45f36d3fdf0e218ce10a89764
Vardhan velamakanni <159052557+VardhanVelamakanni@users.noreply.github.com>
2025-02-02 22:36:53 +0530

    commit basics

---
+++ helloworld.py
@@ -0,0 +1,16 @@
+print("hello world")
+print("hi this is my first program", "hope it goes well")
+print(23)
+name="vardhan"
+age=18
+variable="value"
+price=24
+print("my name is:" ,name)
+print ("my value is:",age+price)
+print(type(name))
+print(type(age))
+a=1000
+b=800
+sum=a+b
+print(sum)
+#comments
