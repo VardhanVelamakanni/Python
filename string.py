2f15626b50a4e0867250ce8a768cf492750c6c56
Vardhan velamakanni <159052557+VardhanVelamakanni@users.noreply.github.com>
2025-02-03 12:20:02 +0530

    commit len strings

--- string.py
+++ string.py
@@ -8,3 +8,16 @@ print(string2)
 #for a tab space btween
 string3="before gap\tafter gap"
 print(string3)
+#conacatenation of strings
+str1="vardhan"
+str2="velamakanni"
+final_str=str1+str2
+print(final_str)
+#length of the string
+str3="vardhanvelamakanni"
+print(len(str3))
+#empty spaces also count while using inbuilt fucntion "len()"
+name1="vardhan"
+name2="velamakanni"
+final_name=name1+ " " +name2
+print(len(final_name))
