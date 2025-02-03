313cfd2f33d74630e7d2b14587a84c161d68ee35
Vardhan velamakanni <159052557+VardhanVelamakanni@users.noreply.github.com>
2025-02-03 21:22:04 +0530

    commit strings over

---
+++ stringdiy.py
@@ -0,0 +1,7 @@
+#write a program to input user's first name and print its length
+string1=input("enter a string")
+print(len(string1))
+
+#write a program to find occurrence of '$' in a string.
+string2=input("enter a string")
+print(string2.count("$"))
