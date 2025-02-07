679d72dbc91ab88a807f1bce78fc449e9c771fa3
Vardhan velamakanni <159052557+VardhanVelamakanni@users.noreply.github.com>
2025-02-07 19:06:45 +0530

    commit dictionary

---
+++ dictionary.py
@@ -0,0 +1,12 @@
+#has key:value pair
+Dictionary={
+    "key":"value",
+    "name":"vardhan",
+    "age":19,
+    "is_adult":True,
+    "marks":80.3,
+    "subjects":["python","c","java"],
+    "topics":("dictionary","tuples")
+
+}
+print(Dictionary)
