ee0280b5d19d27381e6d3d18e645851b168bce88
Vardhan velamakanni <159052557+VardhanVelamakanni@users.noreply.github.com>
2025-02-08 17:56:08 +0530

    commmit dictionaries

--- dictionary.py
+++ dictionary.py
@@ -18,4 +18,32 @@ Dictionary["surname"]="Velamakanni"
 #this can also start with a null dictionary
 null_dictionary={}
 null_dictionary["name"]="vardhan"
-print(null_dictionary)
+#print(null_dictionary)
+#so this way we can start with a null dictionary and keep on adding stuiuf to it
+
+
+#Nested Dictionary
+Student={
+    "name":"vardhan",
+    "score":{
+        "coding":"a",
+        "aptitude":"b",
+        "Maths":"c"
+    },
+    "Cgpa":7
+}
+#print(Student)
+
+#print(Student["score"]["coding"])
+
+#dicitonary Methods
+
+print(list(Student.keys()))#prints all the keys present in the dictionary
+print(list(Student.values()))#prins all the valkues prestnt in the dicitonary as a list
+print(Student.items())#prints all the key value pairs as a tuple
+pairs=list(Student.items())
+print(pairs[0])#prints the provided pair 
+print(Student.get("name"))#prints the vlaue of the key
+#to update the dictionary with a new key and value pair
+Student.update({"City":"Vijayawada"})
+print(Student)
