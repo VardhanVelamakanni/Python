31b44d974157fcadd1a8c529de0ba5c6959c15e2
Vardhan velamakanni <159052557+VardhanVelamakanni@users.noreply.github.com>
2025-02-08 19:04:05 +0530

    commit part 1 diy

---
+++ dictionariessetdiy.py
@@ -0,0 +1,13 @@
+#store folllowoing word meaning in a python dictionary:
+Dictionary={
+    "table":{
+        "a piece of furniture", "list of facts and figures"
+    },
+    "cat":"a small animal"
+}
+print(Dictionary)
+
+Student_subjects={
+    "python","java","C++","python","javascript","java","python","java","C++","C"
+}
+print(Student_subjects)
