5425d9458aa9f08148393a2ae157d5c60c86c819
Vardhan velamakanni <159052557+VardhanVelamakanni@users.noreply.github.com>
2025-02-09 22:05:26 +0530

    commit while loops

---
+++ loops.py
@@ -0,0 +1,15 @@
+#loops are used to repeat instructions
+#While loop
+#for example lets take a example and print hello 5 times
+#print("hello")
+#print("hello")
+#print("hello")
+#print("hello")
+#print("hello")
+#which sort of helps for some lines,but for things to be printed for 1lakh times or more than that the file readability will be decreased and the file storage will be more with i lakh code lines
+#hence for this for and while loop helps a lot
+#example
+count = 1
+while count <=5:
+    print("hello")
+    count += 1
