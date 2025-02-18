c731e894c17afd3df951a855107e7bdde6f13d83
Vardhan velamakanni <159052557+VardhanVelamakanni@users.noreply.github.com>
2025-02-19 01:39:06 +0530

    functions over

--- functions and recursion/functiondiy.py
+++ functions and recursion/functiondiy.py
@@ -11,6 +11,37 @@ def len_list():
     return a
 b=len_list()
 
+#write a program to print elements of a list in a single line
+def sing_line():
+    n=int(input("enter the size of the list"))
+    a=[]
+    for i in range(n):
+        ele=int(input("enter the elements"))
+        i+=1
+        a.append(ele)
+    print(a,end="")
+    return a
+c=sing_line()
+
+#write a program to find factorial of a number
+def facto(n):
+    n=int(input("enter the number"))
+    fact=1
+    for i in range(1,n+1):
+        fact *=i
+    print(fact)
+    
+facto(4)
+
+#write a program to convert USD to INR
+def USd(n):
+    n=int(input("enter the USD value"))
+    m=n*80
+    print("your Indian value is ",m)
+USd(80)
+
+
+
 
 
 
